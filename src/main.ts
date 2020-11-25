512d1640de4e5434ed2036f24ecddc724cff2c49
bananive <fr8ntend@gmail.com>
2020-11-25 11:45:14 +0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const appOptions = {cors: true};
+  const app = await NestFactory.create(AppModule, appOptions);
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
+}
+bootstrap();
